6f14e588c26cbdbca623df45a36f471612e56899
vineet <vineetkr98@gmail.com>
2025-12-20 20:52:43 +0530

    fix: version upgraded

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import { add, multiply } from "./math.js";
 import { capitalize, lowercase } from "./string.js";
 
 export { add, multiply, capitalize, lowercase }
-export default {
-    add,
-    multiply,
-    capitalize,
-    lowercase
-};
