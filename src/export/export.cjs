a55baff79ff3ab2a15eb1b786eb970435c3d38d2
vineet <vineetkr98@gmail.com>
2026-01-12 19:43:48 +0530

    fix: Issue fixed

--- src/export/export.cjs
+++ src/export/export.cjs
@@ -1,3 +1,6 @@
 const math = require('../math')
 const string = require('../string')
-module.exports = {...math, ...string}
+module.exports.add = math.add;
+module.exports.multiply = math.multiply;
+module.exports.capitalize = string.capitalize;
+module.exports.lowercase = string.lowercase;
